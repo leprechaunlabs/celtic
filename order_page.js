47f8160aa6ee55fe24e6200a0ac3b08d2c527e62
Horacio Garcia <horacio@leprechaunpromotions.com>
2020-02-05 12:56:09 -0500

    fix: uncaught error 257

--- order_page.js
+++ order_page.js
@@ -30,8 +30,8 @@ function appendOrderNumbers(data) {
                 <div class="content">\
                     <div class="summary">\
                         <ul class="ui sub header" style="list-style: none;">\
-                        <li>JOB NUMBER:<p>'+data.context.message[0].job_number+'</p></li>\
-                        <li>TRANSACTION NUMBER<p>'+data.context.message[0].po_number+'</p></li>\
+                        <li>JOB NUMBER:<p>'+data.message[0].job_number+'</p></li>\
+                        <li>TRANSACTION NUMBER<p>'+data.message[0].po_number+'</p></li>\
                         <li>PO NUMBER</li>\
                         <li>ORDER DATE</li>\
                         <li>CUSTOMER:</li>\
