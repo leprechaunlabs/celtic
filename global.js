093d6e8a9ebf7ddf4a508012c262932b532a0167
Horacio Garcia <horacio@leprechaunpromotions.com>
2020-02-07 14:55:02 -0500

    update: to loader

--- global.js
+++ global.js
@@ -1,9 +1,14 @@
 var netsuiteURL = "https://4976131-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=391&deploy=1&compid=4976131_SB1&h=bc0412cde19c51afd168";
 var corsHerokuURL = "https://cors-anywhere.herokuapp.com/";
 
+
+
+
 $("#button-order-status-modal").click(function () {
+    $(".horizontalCards").append(
+        '<div class="ui segment"><div class="ui active slow green double loader"></div><br><br><br><br></div>');
     $.getJSON(corsHerokuURL + netsuiteURL, jobNumberOBJ(), function (data, textStatus, jqXHR) {
-        console.log(data);
+        //console.log(data);
         //console.log(jqXHR);
         $(".horizontalCards").empty();
         document.getElementById('input-job-number').value = "";
